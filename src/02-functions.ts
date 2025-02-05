5992004f6051ae6ba9620fb3cd782c3c895291ce
wjc6666666 <2101079563@qq.com>
2025-02-05 18:55:37 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import { Friend, Colleague } from './myTypes';
+import { Friend, Colleague, EmailContact } from './myTypes';
 import { friends, colleagues } from './01-basics';
 function older(f: Friend) : string {
     f.age += 1
@@ -32,3 +32,30 @@ function highestExtension(cs: Colleague[]): Colleague {
     }
     addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+  ): EmailContact[] {
+    const sorted = colleagues.sort(sorter); 
+    const result: EmailContact[] = sorted.map((ce) => ({
+      name: ce.name,
+      email: ce.contact.email
+    }));
+    return result;
+  }
+  
+  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+  
+  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+  function findFriends(
+    friends: Friend[],
+    criteria: (friend: Friend) => boolean
+  ): string[] {
+    return friends.filter(criteria).map(friend => friend.name);
+  }
+  
+  console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+  
+  console.log(findFriends(friends, (friend) => friend.age < 35));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -17,3 +17,7 @@ export interface ColleagueHistory {
   current: Colleague[],
   former: Colleague[]
 }
+export interface EmailContact {
+  name: string;
+  email: string;
+}
