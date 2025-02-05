039b68e6d0f4c3e2e6c93a358b561130deb834ab
wjc6666666 <2101079563@qq.com>
2025-02-05 19:00:36 +0000

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,16 +1,18 @@
 import { Friend, Colleague, ColleagueHistory } from './myTypes';
 
-const friend1: Friend = {
-  name: "Paul Fleming",
-  phone: "087-12345",
-  age: 25,
-};
-
-const friend2: Friend = {
-  name: "Jane Costello",
-  phone: "086--12345",
-  age: 31,
-};
+const friend1 : Friend = {
+    name: "Paul Fleming",
+    phone: "087-12345",
+    age: 25,
+    dob: new Date("1998-11-20") ,
+  };
+  
+  const friend2 : Friend = {
+    name: "Jane Costello",
+    phone: "086--12345",
+    age: 31,
+    interests: ['Music', 'Sport']
+  };
 
 export const friends: Friend[] = [friend1, friend2];
 
--- src/02-functions.ts
+++ src/02-functions.ts
@@ -35,27 +35,21 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number
+    sorter: (c1: Colleague, c2: Colleague) => number,
+    max?: number  
   ): EmailContact[] {
-    const sorted = colleagues.sort(sorter); 
-    const result: EmailContact[] = sorted.map((ce) => ({
+    let end = colleagues.length;
+    if (max !== undefined) {
+      end = max < 2 ? 1 : max;  
+    }
+    const sorted = colleagues.sort(sorter);
+    const fullResult = sorted.map((ce) => ({
       name: ce.name,
       email: ce.contact.email
     }));
-    return result;
+    return fullResult.slice(0, end);
   }
   
-  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-  
+  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension, 3));
+  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length, 1));
   console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
-
-  function findFriends(
-    friends: Friend[],
-    criteria: (friend: Friend) => boolean
-  ): string[] {
-    return friends.filter(criteria).map(friend => friend.name);
-  }
-  
-  console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
-  
-  console.log(findFriends(friends, (friend) => friend.age < 35));
